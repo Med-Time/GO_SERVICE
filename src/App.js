d456f08ea727984eaddfe92eaf3c80ffdcc96b56
Bhishan Pangeni <bhishan.cd22@bmsce.ac.in>
2024-01-14 11:23:23 +0530

    First Upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <p>
+          Welcome to Go Service. This is a test for CI/CD.
+        </p>
+          Test By Bhishan Pangeni
+    </div>
+  );
+}
+
+export default App;
