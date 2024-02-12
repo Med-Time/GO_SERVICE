b17865459833c79c00e5292c80cc74a1c4021774
Bhishan Pangeni <bhishan.cd22@bmsce.ac.in>
2024-02-13 01:00:21 +0530

    Update Login
    Footer changed

--- src/components/Login.js
+++ src/components/Login.js
@@ -30,7 +30,7 @@ export default function Login() {
                         </div>
                         <button className="btn btn-primary w-25 py-2" type="submit">Log in</button>
                         <button className="btn btn-success w-50 py-2 mx-2" type="submit">Create new Account</button>
-                        <p className="mt-5 mb-3 text-body-secondary">© 2024</p>
+                        <p className="mt-5 mb-3 text-body-secondary">Go Service © 2024</p>
                     </form>
                 </div>
             </div>
