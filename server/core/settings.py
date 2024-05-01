625a4ccc6cf2f9714b1eb715517ffe6bc44415c2
Anmol Bhusal <153626768+anmol52490@users.noreply.github.com>
2024-05-01 12:45:38 +0545

    Updated settings.py environment variables

--- server/core/settings.py
+++ server/core/settings.py
@@ -143,5 +143,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'parikshitmaharjan78@gmail.com'
-EMAIL_HOST_PASSWORD = 'anci mrcw crcq iqth'
+EMAIL_HOST_USER = os.environ.get['EMAIL_HOST_USER']
+EMAIL_HOST_PASSWORD = os.environ.get['EMAIL_HOST_PASSWORD']
